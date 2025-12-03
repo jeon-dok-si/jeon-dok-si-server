430124e4839df0bed8085dad514911d75ea9dbdb
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-12-03 20:13:57 +0900

    fix: 길드 목록 로직 수정

--- src/main/java/com/jeondoksi/jeondoksi/domain/guild/dto/CreateGuildRequest.java
+++ src/main/java/com/jeondoksi/jeondoksi/domain/guild/dto/CreateGuildRequest.java
@@ -9,6 +9,7 @@ public class CreateGuildRequest {
     private String name;
     private String description;
     private int maxMembers;
+    @com.fasterxml.jackson.annotation.JsonProperty("isPrivate")
     private boolean isPrivate;
     private String password;
     private boolean generateJoinCode;
